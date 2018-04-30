c7d587e631f79eb255b18c9624941e33c6365aa2
dylan-jannetty <djannetty29@gmail.com>
2018-04-30 14:35:27 -0400

    changed required
    changed require teamui to booksui
    changed require teamapi to bookapi
    
    index.js teamEvents to bookEvents

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -5,7 +5,7 @@
 
 // use require without a reference to ensure a file is bundled
 // require('./example')
-const teamEvents = require('./team/teamevents')
+const bookEvents = require('./books/bookevents')
 $(() => {
-  teamEvents.addHandlers()
+  bookEvents.addHandlers()
 })
